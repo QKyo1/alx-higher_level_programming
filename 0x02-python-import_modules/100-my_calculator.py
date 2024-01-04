f132d859f07f4c79df0e9a580ba86702b4e328f9
qkyo1 <engawab8@gmai.com>
2024-01-05 02:59:52 +0400

    all

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as calc
+    from sys import argv
+    x = ["+", "-", "*", "/"]
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    elif not argv[2] in x:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print("{} + {} = {}".format(a, b, calc.add(a, b)))
+        elif argv[2] == "-":
+            print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+        elif argv[2] == "*":
+            print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+        elif argv[2] == "/":
+            print("{} / {} = {}".format(a, b, calc.div(a, b)))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    
+    from sys import argv
+    x = len(argv)
+    y = "argument" if x == 2 else "arguments"
+    print("{} {}{}".format(x - 1, y, "." if x == 1 else ":"))
+    if x > 1:
+        for i in range(1, x):
+            print("{}: {}".format(i, argv[i]))
