0a21ee3d88fba69652d07eac892ccd43b87d2da3
qkyo1 <engawab8@gmai.com>
2024-01-09 05:46:13 +0400

    aallll

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    c = (len(sentence), sentence[0] if sentence else None)
+    return c
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    maximum = my_list[0]
+    for i in my_list:
+        maximum = i if i > maximum else maximum
+    return maximum
