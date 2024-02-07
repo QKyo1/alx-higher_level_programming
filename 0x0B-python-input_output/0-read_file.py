c1b5dc7035783747568a5f5f36a2550ea7eb9a9b
qkyo1 <engawab8@gmail.com>
2024-02-07 22:13:37 +0400

    alllll

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+jokes on you
+"""
+
+
+def read_file(filename=""):
+    """
+    jokes on you
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+jokes on youu
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    jokes on you
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
