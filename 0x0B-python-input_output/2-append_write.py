7d607dc9d54d923f5d593cd1e5a396e84e620c2f
qkyo1 <engawab8@gmail.com>
2024-02-07 22:19:12 +0400

    hhf

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+jokes on you
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    jokes on youu
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+jokes on you
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    jokes on you
+    """
+    return json.dumps(my_obj)
