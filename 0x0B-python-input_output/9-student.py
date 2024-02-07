f7eb89948d439569f1b01668e18d4f4bfeac78bd
qkyo1 <engawab8@gmail.com>
2024-02-07 22:23:28 +0400

    fdsdfg

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+jokes on oyuu
+"""
+
+
+class Student:
+    """
+    student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        jokess on oyu
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        jokes on oyu
+        """
+        return self.__dict__.copy()
+    
